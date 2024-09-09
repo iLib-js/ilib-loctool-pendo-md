877e7f38e750b0eee2f00039de74f35cffaf0648
Wadim Wawrzenczak <wwawrzenczak@box.com>
2024-09-10 01:50:33 +0200

    Remove position info from parsed md

--- src/markdown/convert.ts
+++ src/markdown/convert.ts
@@ -2,16 +2,20 @@ import fromMarkdown from "mdast-util-from-markdown";
 import strikethrough from "mdast-util-gfm-strikethrough";
 import toMarkdown from "mdast-util-to-markdown";
 import strikethroughSyntax from "micromark-extension-gfm-strikethrough";
+import unistUtilRemovePosition from "unist-util-remove-position";
 import underlineSyntax from "./plugin/underline/syntax";
 import underline from "./plugin/underline/mdast";
 
 import type { Root } from "mdast";
 
-export const parse = (markdown: string): Root =>
-    fromMarkdown(markdown, {
+export const parse = (markdown: string): Root => {
+    const ast = fromMarkdown(markdown, {
         extensions: [strikethroughSyntax({ singleTilde: false }), underlineSyntax()],
         mdastExtensions: [strikethrough.fromMarkdown, underline.fromMarkdown],
     });
+
+    return unistUtilRemovePosition(ast) as Root;
+};
 
 export const stringify = (tree: Root): string =>
     toMarkdown(tree, { extensions: [strikethrough.toMarkdown, underline.toMarkdown] });
