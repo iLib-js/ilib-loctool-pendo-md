0cf2deea041ae27da3d367f7e9f02ff8223ea44d
Wadim Wawrzenczak <wwawrzenczak@box.com>
2024-09-11 01:11:52 +0200

    * Backconvert color nodes * Enable color ast transform in main convert interface * More tests

--- src/markdown/ast-transformer/__tests__/color.test.ts
+++ src/markdown/ast-transformer/__tests__/color.test.ts
@@ -1,4 +1,4 @@
-import { toColorNodes } from "../color";
+import { fromColorNodes, toColorNodes } from "../color";
 
 import fromMarkdown from "mdast-util-from-markdown";
 import unistUtilRemovePosition from "unist-util-remove-position";
@@ -11,8 +11,26 @@ const toAst = (markdown: string): Root => {
 
 describe("ast-transformer/color", () => {
     describe("toColorNodes", () => {
-        it("should reduce HTML color nodes to custom Color nodes", () => {
-            const parsed = toAst('normal text <color value="#000000">colored text</color> normal text');
+        it("should not modify the AST if there are no color nodes", () => {
+            const parsedAst = toAst("normal text");
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [{ type: "text", value: "normal text" }],
+                    },
+                ],
+            };
+
+            const actual = toColorNodes(parsedAst);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it("should reduce HTML color nodes span to a custom Color node", () => {
+            const parsedAst = toAst('normal text <color value="#000000">colored text</color> normal text');
 
             const expected = {
                 type: "root",
@@ -39,7 +57,276 @@ describe("ast-transformer/color", () => {
                     },
                 ],
             };
-            expect(toColorNodes(parsed)).toEqual(expected);
+
+            const actual = toColorNodes(parsedAst);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it("should handle multiple color nodes", () => {
+            const parsedAst = toAst(
+                'normal text <color value="#000000">colored text</color> normal text <color value="#ffffff">colored text</color> normal text',
+            );
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            { type: "text", value: "normal text " },
+                            {
+                                type: "color",
+                                value: "#000000",
+                                children: [{ type: "text", value: "colored text" }],
+                            },
+                            { type: "text", value: " normal text " },
+                            {
+                                type: "color",
+                                value: "#ffffff",
+                                children: [{ type: "text", value: "colored text" }],
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+
+            const actual = toColorNodes(parsedAst);
+
+            expect(actual).toEqual(expected);
+        });
+
+        // not sure if nesting is actully supported in Pendo
+        it("should handle nested color nodes", () => {
+            const parsedAst = toAst(
+                'normal text <color value="#000000">colored text <color value="#ffffff">colored text</color></color> normal text',
+            );
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            { type: "text", value: "normal text " },
+                            {
+                                type: "color",
+                                value: "#000000",
+                                children: [
+                                    { type: "text", value: "colored text " },
+                                    {
+                                        type: "color",
+                                        value: "#ffffff",
+                                        children: [{ type: "text", value: "colored text" }],
+                                    },
+                                ],
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+
+            const actual = toColorNodes(parsedAst);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it("should handle adjacent color nodes", () => {
+            const parsedAst = toAst(
+                'normal text <color value="#000000">colored text</color><color value="#ffffff">colored text</color> normal text',
+            );
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            { type: "text", value: "normal text " },
+                            {
+                                type: "color",
+                                value: "#000000",
+                                children: [{ type: "text", value: "colored text" }],
+                            },
+                            {
+                                type: "color",
+                                value: "#ffffff",
+                                children: [{ type: "text", value: "colored text" }],
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+
+            const actual = toColorNodes(parsedAst);
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe("fromColorNodes", () => {
+        it("should backconvert custom Color nodes to HTML color nodes", () => {
+            const ast = toAst('normal text <color value="#000000">colored text</color> normal text');
+            const astWithColorNodes = toColorNodes(ast);
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            {
+                                type: "text",
+                                value: "normal text ",
+                            },
+                            {
+                                type: "html",
+                                value: '<color value="#000000">',
+                            },
+                            {
+                                type: "text",
+                                value: "colored text",
+                            },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+            const actual = fromColorNodes(astWithColorNodes);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it("should backconvert multiple color nodes", () => {
+            const ast = toAst(
+                'normal text <color value="#000000">colored text</color> normal text <color value="#ffffff">colored text</color> normal text',
+            );
+            const astWithColorNodes = toColorNodes(ast);
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            { type: "text", value: "normal text " },
+                            {
+                                type: "html",
+                                value: '<color value="#000000">',
+                            },
+                            { type: "text", value: "colored text" },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            { type: "text", value: " normal text " },
+                            {
+                                type: "html",
+                                value: '<color value="#ffffff">',
+                            },
+                            { type: "text", value: "colored text" },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+            const actual = fromColorNodes(astWithColorNodes);
+
+            expect(actual).toEqual(expected);
+        });
+
+        // not sure if nesting is actully supported in Pendo
+        it("should handle nested color nodes", () => {
+            const ast = toAst(
+                'normal text <color value="#000000">colored text <color value="#ffffff">colored text</color></color> normal text',
+            );
+            const astWithColorNodes = toColorNodes(ast);
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            { type: "text", value: "normal text " },
+                            {
+                                type: "html",
+                                value: '<color value="#000000">',
+                            },
+                            { type: "text", value: "colored text " },
+                            {
+                                type: "html",
+                                value: '<color value="#ffffff">',
+                            },
+                            { type: "text", value: "colored text" },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+            const actual = fromColorNodes(astWithColorNodes);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it("should handle adjacent color nodes", () => {
+            const ast = toAst(
+                'normal text <color value="#000000">colored text</color><color value="#ffffff">colored text</color> normal text',
+            );
+            const astWithColorNodes = toColorNodes(ast);
+
+            const expected = {
+                type: "root",
+                children: [
+                    {
+                        type: "paragraph",
+                        children: [
+                            { type: "text", value: "normal text " },
+                            {
+                                type: "html",
+                                value: '<color value="#000000">',
+                            },
+                            { type: "text", value: "colored text" },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            {
+                                type: "html",
+                                value: '<color value="#ffffff">',
+                            },
+                            { type: "text", value: "colored text" },
+                            {
+                                type: "html",
+                                value: "</color>",
+                            },
+                            { type: "text", value: " normal text" },
+                        ],
+                    },
+                ],
+            };
+            const actual = fromColorNodes(astWithColorNodes);
+
+            expect(actual).toEqual(expected);
         });
     });
 });
--- src/markdown/string-transformer/__tests__/color.test.ts
+++ src/markdown/string-transformer/__tests__/color.test.ts
@@ -1,18 +1,64 @@
-import { toXmlTags, toMarkdown } from "../color";
+import { toHtmlTags, fromHtmlTags } from "../color";
 
-describe("transformer/color/string", () => {
+describe("string-transformer/color", () => {
     describe("toXmlTags", () => {
         it("should replace color tags with XML-like tags", () => {
             const markdown = "{color: #000000}colored text{/color}";
             const expected = '<color value="#000000">colored text</color>';
-            expect(toXmlTags(markdown)).toBe(expected);
+            expect(toHtmlTags(markdown)).toBe(expected);
+        });
+
+        it("should replace multiple color tags with XML-like tags", () => {
+            const markdown =
+                "regular text {color: #000000}colored text{/color} regular text {color: #ffffff}colored text{/color} regular text";
+            const expected =
+                'regular text <color value="#000000">colored text</color> regular text <color value="#ffffff">colored text</color> regular text';
+            expect(toHtmlTags(markdown)).toBe(expected);
+        });
+
+        // not sure if Pendo supports nested color tags
+        it("should replace nested color tags with XML-like tags", () => {
+            const markdown =
+                "{color: #000000}colored text {color: #ffffff}nested colored text{/color} nested colored text{/color}";
+            const expected =
+                '<color value="#000000">colored text <color value="#ffffff">nested colored text</color> nested colored text</color>';
+            expect(toHtmlTags(markdown)).toBe(expected);
+        });
+
+        it("should replace adjacent color tags with XML-like tags", () => {
+            const markdown = "{color: #000000}colored text{/color}{color: #ffffff}colored text{/color}";
+            const expected = '<color value="#000000">colored text</color><color value="#ffffff">colored text</color>';
+            expect(toHtmlTags(markdown)).toBe(expected);
         });
     });
-    describe("toMarkdown", () => {
+    describe("fromHtmlTags", () => {
         it("should replace XML-like tags with color tags", () => {
             const xml = '<color value="#000000">colored text</color>';
             const expected = "{color: #000000}colored text{/color}";
-            expect(toMarkdown(xml)).toBe(expected);
+            expect(fromHtmlTags(xml)).toBe(expected);
+        });
+
+        it("should replace multiple XML-like tags with color tags", () => {
+            const xml =
+                'regular text <color value="#000000">colored text</color> regular text <color value="#ffffff">colored text</color> regular text';
+            const expected =
+                "regular text {color: #000000}colored text{/color} regular text {color: #ffffff}colored text{/color} regular text";
+            expect(fromHtmlTags(xml)).toBe(expected);
+        });
+
+        // not sure if Pendo supports nested color tags
+        it("should replace nested XML-like tags with color tags", () => {
+            const xml =
+                '<color value="#000000">colored text <color value="#ffffff">nested colored text</color> nested colored text</color>';
+            const expected =
+                "{color: #000000}colored text {color: #ffffff}nested colored text{/color} nested colored text{/color}";
+            expect(fromHtmlTags(xml)).toBe(expected);
+        });
+
+        it("should replace adjacent XML-like tags with color tags", () => {
+            const xml = '<color value="#000000">colored text</color><color value="#ffffff">colored text</color>';
+            const expected = "{color: #000000}colored text{/color}{color: #ffffff}colored text{/color}";
+            expect(fromHtmlTags(xml)).toBe(expected);
         });
     });
 });
