956b0bb9f90e4a53f7259cc9d3cd10a1a0f03b38
Wadim Wawrzenczak <wwawrzenczak@box.com>
2024-09-12 17:07:33 +0200

    More tests for the component ast transformer (and some bugfixes)

--- src/markdown/ast-transformer/component/mapping.ts
+++ src/markdown/ast-transformer/component/mapping.ts
@@ -11,76 +11,7 @@ import type {
 
 import type { Color as MColor } from "../color/color";
 import type { Underline as MUnderline } from "../../micromark-plugin/underline";
-
-interface BaseComponentData {
-    type: string;
-}
-
-interface CBold extends BaseComponentData {
-    type: "strong";
-}
-
-interface CItalic extends BaseComponentData {
-    type: "emphasis";
-}
-
-interface CUnderline extends BaseComponentData {
-    type: "underline";
-}
-
-interface CStrikethrough extends BaseComponentData {
-    type: "delete";
-}
-
-interface CLink extends BaseComponentData {
-    type: "link";
-    url: string;
-}
-
-interface CList extends BaseComponentData {
-    type: "list";
-    ordered: boolean;
-}
-
-interface CListItem extends BaseComponentData {
-    type: "listItem";
-}
-
-interface CColor extends BaseComponentData {
-    type: "color";
-    value: string;
-}
-
-interface CHTML extends BaseComponentData {
-    type: "html";
-    value: string;
-}
-
-/**
- * Possible component data types.
- */
-interface ComponentDataMap {
-    strong: CBold;
-    emphasis: CItalic;
-    underline: CUnderline;
-    delete: CStrikethrough;
-    link: CLink;
-    list: CList;
-    listItem: CListItem;
-    color: CColor;
-    html: CHTML;
-}
-
-/**
- * Holds data about substituted markdown syntax (i.e. mdast node) which allows it to be recreated later.
- */
-export type ComponentData = ComponentDataMap[keyof ComponentDataMap];
-
-/**
- * Sequence of components that were substituted in the mdast tree
- * (in the order of tree traversal).
- */
-export type ComponentList = ComponentData[];
+import { ComponentData } from ".";
 
 /**
  * Create a component data representation from a mdast node.
