bf79939ed98aa81f51d75dde334447698bdd6bef
Wadim Wawrzenczak <wwawrzenczak@box.com>
2024-09-12 14:22:11 +0200

    Clean up ast transformer color tests and exports

--- src/markdown/ast-transformer/color/index.ts
+++ src/markdown/ast-transformer/color/index.ts
@@ -1,5 +1,13 @@
-export { type Color } from "./color";
-
+import type { Color } from "./color";
 import { fromColorNodes, toColorNodes } from "./color";
 
+// extend available nodes in mdast
+// as described in JSDoc of @types/mdast@3.0.15
+declare module "mdast" {
+    interface PhrasingContentMap {
+        Color: Color;
+    }
+}
+
+export { Color };
 export default { fromColorNodes, toColorNodes };
