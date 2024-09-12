2288bbb040bd2f7153d07f7b40d581b934d2b6b0
Wadim Wawrzenczak <wwawrzenczak@box.com>
2024-09-12 13:47:12 +0200

    Fix underline plugin node type extension

--- src/markdown/micromark-plugin/underline/index.ts
+++ src/markdown/micromark-plugin/underline/index.ts
@@ -1,7 +1,17 @@
 import mdast from "./mdast";
 import syntax from "./syntax";
+import type { Underline } from "./mdast";
 
-export type { Underline } from "./mdast";
+// extend available nodes in mdast types
+// as described in JSDoc of @types/mdast@3.0.15
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/e9a0a9a3fae552bff4df6fbad1a05f3ce45fcc4a/types/mdast/v3/index.d.ts#L73
+declare module "mdast" {
+    interface StaticPhrasingContentMap {
+        underline: Underline;
+    }
+}
+
+export type { Underline };
 
 export default {
     mdastEstension: mdast,
