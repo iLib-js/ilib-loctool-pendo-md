2a6e6e3f24d25142902e8d9453f4f1d2ab4d0273
Wadim Wawrzenczak <wwawrzenczak@box.com>
2024-09-23 23:21:05 +0200

    Eslint no-unused-vars ignore underscores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,4 +23,20 @@ export default tseslint.config(
         },
     },
     prettier,
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    },
 );
